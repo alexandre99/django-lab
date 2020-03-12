13ca211375f8cc7ca5a68b0aa59680a5b8ca63a2
Alexandre Teixeira <alexanddy99@gmail.com>
2020-03-12 08:55:14 -0300

    exibindo dados do bd

--- receitas/views.py
+++ receitas/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
+from .models import Receita
 
 def index(request):
-  receitas = {
-    1: 'Lasanha',
-    2: 'Sopa de legumes',
-    3: 'Sorvete'
-  }
-  dados = {
-    'nomes_das_receitas': receitas
-  }
-  return render(request, 'index.html', dados)
+    receitas_bd = Receita.objects.all()
+
+    dados = {
+        'receitas': receitas_bd
+    }
+    return render(request, 'index.html', dados)
 
 def receitas(request):
-  return render(request, 'receita.html')
+    return render(request, 'receita.html')
