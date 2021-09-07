02c7dd81cd353ba319fd5eac394f4e8175c66745
italo.teixeira <italo.teixeira@ifood.com.br>
2021-09-07 16:10:54 -0300

    update repository

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/alexandreteixeira/alexandre.teixeira/python3/django-alura/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
